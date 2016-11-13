4f9708342c7f5ec9c31044a45a36023a7b70cc16
liveforensics <liveforensics@mark-morley.co.uk>
2016-11-13 12:59:58 +0000

    new design hopefully will push this time

---
+++ MemoryExplorer/Model/BusinessLogic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryExplorer.Model
+{
+    public partial class DataModel : INotifyPropertyChanged, IDisposable
+    {
+        // new design starts here..
+    }
+}
